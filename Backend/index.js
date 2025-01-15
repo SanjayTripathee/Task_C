f4210e2afae83ad5ec5285e5829d9fb6a219aa25
sanjay <st1706851@gmail.com>
2025-01-15 06:58:17 +0545

    creating schema and installing package needed for project

--- Backend/index.js
+++ Backend/index.js
@@ -1,1 +1,31 @@
-console.log("Sanjay")
+// import cors from "cors";
+// import express, { json } from "express";
+// import connectToMongoDb from "./src/connectToMongoDb/connectToMongoDb.js";
+// import webUserRouter from "./src/routes/webUserRouter.js";
+// import fileRouter from "./src/routes/fileRouter.js";
+// import notFoundMiddleware from "./src/middlewire/notFoundMiddleware.js";
+// import errorMiddleware from "./src/middlewire/errorMiddleware.js";
+
+// // import errorMiddleware from "./src/middlewire/errorMiddlewire.js";
+// // import notFoundMiddleware from "./src/middlewire/notFoundMiddleware.js";
+
+
+// let expressApp = express();
+// expressApp.use(cors()); //! cors is enable so that we could hit api from browser(it is always put on top)
+// expressApp.use(json()); //dont forget to import json
+
+// expressApp.listen(8000, () => {
+//   console.log("Server is running on port 8000");
+//   connectToMongoDb();
+// });
+
+// expressApp.use("/web-users", webUserRouter); //write kabab case on sending part ok
+
+
+// expressApp.use("/file", fileRouter);
+
+// expressApp.use(express.static("./public"));
+
+// expressApp.use("*", notFoundMiddleware); //! always put it after all route is decleare ..put down after route ok...
+
+// expressApp.use(errorMiddleware);
