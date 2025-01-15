bec2f86d80e6de0f16a959d560f22c07124f23e8
sanjay <st1706851@gmail.com>
2025-01-15 10:14:52 +0545

    some change

--- vite-project/src/ReactRouter.jsx
+++ vite-project/src/ReactRouter.jsx
@@ -2,19 +2,11 @@ import React from "react";
 import { Outlet, Route, Routes } from "react-router-dom";
 import Register from "./component/webUsers/Register";
 
-
 const ReactRouter = () => {
   return (
     <div>
       <Routes>
-        <Route path="/" element={<Register/>}>
-          {/* index on a Route, it means that this route will be rendered when the parent route's path is matched exactly. */}
-
-          <Route path="admin" element={<Outlet />}>
-            <Route index element={<div>This is admin dashBoard</div>}></Route>
-            <Route path="register" element={<Register />}></Route>
-          </Route>
-        </Route>
+        <Route path="/" element={<Register />}></Route>
 
         <Route path="*" element={<div>404 Page</div>}></Route>
       </Routes>
